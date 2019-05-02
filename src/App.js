f249154f56a28df81d68d39f0135d0dfbbc4bd8e
Dennis Gakenholz <m00ri@web.de>
2019-05-02 14:53:45 +0200

    deleted test nav link

--- src/App.js
+++ src/App.js
@@ -26,7 +26,6 @@ class App extends Component {
 								<Route exact path='/contact/add' component={AddContact} />
 								<Route exact path='/contact/edit/:id' component={EditContact} />
 								<Route exact path='/about' component={About} />
-								<Route exact path='/test' component={Test} />
 								<Route component={NotFound} />
 							</Switch>
 						</div>
