5e9751ffd56c1afa490d252178261a1e6fc0cd69
Dennis Gakenholz <m00ri@web.de>
2019-05-02 14:49:27 +0200

    Updates

---
+++ precache-manifest.0c8569843312651bb661a14673980266.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7d9fee7e0f266f0d9505",
+    "url": "/contactmanager/static/css/main.0c0ee287.chunk.css"
+  },
+  {
+    "revision": "7d9fee7e0f266f0d9505",
+    "url": "/contactmanager/static/js/main.f2698d09.chunk.js"
+  },
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  },
+  {
+    "revision": "232ecce2fa6f081bdd82",
+    "url": "/contactmanager/static/css/2.512c3a8c.chunk.css"
+  },
+  {
+    "revision": "232ecce2fa6f081bdd82",
+    "url": "/contactmanager/static/js/2.62ff94e0.chunk.js"
+  },
+  {
+    "revision": "9992c5ebbed9a0346edfe6fb477f2746",
+    "url": "/contactmanager/index.html"
+  }
+];
