f2e5d6ec053a661b0e342a1f3ff0f1a893004a7d
Dennis Gakenholz <dennis.gakenholz@edicos.de>
2019-05-02 14:18:27 +0200

    initial commit

---
+++ src/components/layout/Header.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { NavLink } from 'react-router-dom'
+
+const Header = props => {
+	const { branding } = props
+	return (
+		<nav className='navbar navbar-expand-sm navbar-dark bg-danger mb-3 py-0'>
+			<div className='container'>
+				<a href='/' className='navbar-brand'>
+					{branding}
+				</a>
+				<div>
+					<ul className='navbar-nav mr-auto'>
+						<li className='nav-item'>
+							<NavLink exact to='/' className='nav-link'>
+								<i className='fas fa-home' /> Home
+							</NavLink>
+						</li>
+						<li className='nav-item'>
+							<NavLink to='/contact/add' className='nav-link'>
+								<i className='fas fa-plus mr-1' />
+								Add
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to='/about' className='nav-link'>
+								<i className='fas fa-question mr-1' />
+								About
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to='/test' className='nav-link'>
+								<i className='fas fa-check mr-1' />
+								Test
+							</NavLink>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	)
+}
+
+Header.defaultProps = {
+	branding: 'My App'
+}
+
+Header.propTypes = {
+	branding: PropTypes.string.isRequired
+}
+
+export default Header
