493c4253a416f5348618d91cae06c78cb4dc6411
Dennis Gakenholz <m00ri@web.de>
2019-05-02 15:29:54 +0200

    changed branding

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -7,9 +7,9 @@ const Header = props => {
 	return (
 		<nav className='navbar navbar-expand-sm navbar-dark bg-danger mb-3 py-0'>
 			<div className='container'>
-				<NavLink exact to='/' className='navbar-brand'>
+				<a href='#/' className='navbar-brand'>
 					{branding}
-				</NavLink>
+				</a>
 				<div>
 					<ul className='navbar-nav mr-auto'>
 						<li className='nav-item'>
